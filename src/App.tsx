c087ccc1411c5952ddce71e0081a4f6589f642ea
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2022-06-27 23:55:45 +0900

    [ feature ] chakuraUIの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Home } from './pages/Home/home';
 
 function App() {
-    return <div className="App"></div>;
+    return <Home></Home>;
 }
 
 export default App;
---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+
+export const Home = () => {
+    return (
+        <Button colorScheme="teal" size="md">
+            Button
+        </Button>
+    );
+};
