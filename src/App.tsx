3f43014778e0e4533fc767c04a39ef045c52a6a3
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2022-06-29 17:33:55 +0900

    [ feature ] ログインコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Home } from './pages/Home/home';
+import { Login } from './pages/login/Login';
 import { Profile } from './pages/profile/Profile';
 
 function App() {
-    return <Profile />;
+    return <Login />;
 }
 
 export default App;
