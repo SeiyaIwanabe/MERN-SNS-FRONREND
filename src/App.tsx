1288ff3755efa853834e3e1ddd62f6990cb2f75b
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2022-06-29 23:54:05 +0900

    [ feature ] react-router-domの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Home } from './pages/Home/home';
 import { Login } from './pages/login/Login';
 import { Profile } from './pages/profile/Profile';
 import { Register } from './pages/register/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-    return <Home />;
-}
+const App: FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/register" element={<Register />}></Route>
+                <Route path="/profile/:username" element={<Profile />}></Route>
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
